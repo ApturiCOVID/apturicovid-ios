17ac61050ac13a8dbb1952d2feea71ed2fbb7e80
Melānija Grunte <melanija.grunte@gmail.com>
2020-05-18 18:44:23 +0300

    Localize PhoneInfoVC and PhoneSettingsVC

--- apturicovid/phoneSettings/PhoneInfoVC.swift
+++ apturicovid/phoneSettings/PhoneInfoVC.swift
@@ -1,7 +1,19 @@
 import UIKit
 
 class PhoneInfoVC: BaseViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var privacyProtectionLabel: UILabel!
+    @IBOutlet weak var closeButton: RoundedButton!
+    
     @IBAction func onBackTap(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func translate() {
+        titleLabel.text = "why_submit_a_phone_number".translated
+        descriptionLabel.text = "why_submit_a_phone_number_explanation".translated
+        privacyProtectionLabel.text = "data_privacy_and_protection_policy".translated
+        closeButton.setTitle("close".translated, for: .normal)
     }
 }

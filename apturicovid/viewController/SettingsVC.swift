dcdd0751974d9ac4f340be72d1fca765d2361457
Melānija Grunte <melanija.grunte@gmail.com>
2020-05-18 18:09:14 +0300

    Add SettingsVC localization

--- apturicovid/viewController/SettingsVC.swift
+++ apturicovid/viewController/SettingsVC.swift
@@ -4,7 +4,16 @@ import RxSwift
 import RxCocoa
 
 class SettingsViewController: BaseViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var spkcCodeDescriptionLabel: UILabel!
+    @IBOutlet weak var submitButton: RoundedButton!
+    @IBOutlet weak var communicationContactLabel: UILabel!    
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var notSpecifiedLabel: UILabel!
+    @IBOutlet weak var specifyButton: UIButton!
+    @IBOutlet weak var contactDescriptionLabel: UILabel!
+    @IBOutlet weak var notifyMeLabel: UILabel!
+    @IBOutlet weak var deleteDataLabel: UILabel!
     @IBOutlet weak var mainStackView: UIStackView!
     @IBOutlet weak var phoneView: UIView!
     @IBOutlet weak var setupPhoneView: UIView!
@@ -12,10 +21,8 @@ class SettingsViewController: BaseViewController {
     @IBOutlet weak var reminderSwitch: UISwitch!
     @IBOutlet weak var languagesStack: UIStackView!
     @IBOutlet weak var versionLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var submitButton: RoundedButton!
     @IBOutlet weak var deleteDataView: UIView!
-    
+
     let langViews = Language.allCases.map{ LanguageView.create($0) }
     
     @IBAction func onReminderSet(_ sender: UISwitch) {
@@ -99,8 +106,17 @@ class SettingsViewController: BaseViewController {
     }
     
     override func translate() {
-        titleLabel.text = "settings_title".translated
-        submitButton.setTitle("settings_enter_code".translated, for: .normal)
+        titleLabel.text = "settings".translated
+        spkcCodeDescriptionLabel.text = "enter_spkc_code".translated
+        submitButton.setTitle("go_to_spkc_code_entry".translated, for: .normal)
+        communicationContactLabel.text = "communication_contact".translated
+        notSpecifiedLabel.text = "not_specified".translated
+        specifyButton.setTitle("specify".translated, for: .normal)
+        changeButton.setTitle("change".translated, for: .normal)
+        contactDescriptionLabel.text = "not_specified_phone_number_limitations".translated
+        notifyMeLabel.text = "notify_if_tracking_isnt_working".translated
+        deleteDataLabel.text = "delete_all_stored_data".translated
+        phoneLabel.text = "SKALFNS"
         submitButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         submitButton.sizeToFit()
     }

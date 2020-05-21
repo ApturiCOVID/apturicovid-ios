98f82a5da54cbeaf7e77253b9bced8f256fc63fe
Mazen Zibara <mazen.zibara@makit.lv>
2020-05-21 19:05:34 +0300

    Switch to v1 for batch download

--- environments/production/env-production.swift
+++ environments/production/env-production.swift
@@ -2,4 +2,4 @@ import Foundation
 
 let baseUrl = "https://apturicovid-staging-api.spkc.gov.lv/api/v1"
 let filesBaseUrl = "https://apturicovid-staging-files.spkc.gov.lv"
-let exposureFilesBaseUrl = "\(filesBaseUrl)/dkfs/v0"
+let exposureFilesBaseUrl = "\(filesBaseUrl)/dkfs/v1"

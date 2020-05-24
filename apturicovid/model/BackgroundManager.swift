649e3f92d62e168311ba3e46c6bf31ef55ea6409
Mazen Zibara <mazen.zibara@makit.lv>
2020-05-24 22:46:51 +0300

    Upload exposures after user saved his phone number

--- apturicovid/model/BackgroundManager.swift
+++ apturicovid/model/BackgroundManager.swift
@@ -1,33 +1,15 @@
 import UIKit
 import CocoaLumberjack
+import RxSwift
 
 class BackgroundManager {
     static let shared = BackgroundManager()
     
+    let disposeBag = DisposeBag()
+    
     func scheduleExposureUploadTask() {
-//        var task = scheduleTask()
-//        ExposuresClient.shared.uploadExposures { (result) in
-//            switch result {
-//            case let .failure(error):
-//                DDLogError(error.localizedDescription)
-//            default: break
-//            }
-//            self.invalidateTask(&task)
-//        }
-    }
-    
-    private func scheduleTask() -> UIBackgroundTaskIdentifier {
-        var task: UIBackgroundTaskIdentifier!
-        task = UIApplication.shared.beginBackgroundTask {
-            self.invalidateTask(&task)
-        }
-        assert(task != .invalid)
-        return task
-    }
-    
-    func invalidateTask(_ task: inout UIBackgroundTaskIdentifier) {
-        DDLogInfo("invalidating task \(task)")
-        UIApplication.shared.endBackgroundTask(task)
-        task = .invalid
+        ExposuresClient.shared.uploadExposures()
+            .subscribe(onError: justPrintError)
+            .disposed(by: disposeBag)
     }
 }
